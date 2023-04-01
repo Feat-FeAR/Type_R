8eb0037297439e6cd6b02d40dbf4d3e5cc9d96ca
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2023-04-01 22:59:12 +0200

    Added some packages to `ReinstR.R`.

--- ReinstR.R
+++ ReinstR.R
@@ -14,12 +14,15 @@ BiocManager::install()
 
 # Base Bioconductor Packages
 BP_level_1 <- c(
-  "Biobase",
-  "MatrixGenerics", # Summary Statistics for  Matrix-Like Objects - rowMedians()
+  "Biobase", # Base Functions for Bioconductor
+  "MatrixGenerics", # Summary Statistics for Matrix-Like Objects - rowMedians()
   "preprocessCore",# Normalization by Quantile-Quantile Algorithm
-  "limma", # Empirical Bayes Method for Differential Expression
-  "RankProd", # Rank Product Method for Differential Expression
-  "genefilter") # Expression Gene Filtering
+  "limma", # Empirical Bayes Method for Differential Expression Analysis
+  "RankProd", # Rank Product Method for Differential Expression Analysis
+  "genefilter", # Gene Expression Filtering
+  "futile.logger", # Logging Utility for R
+  "VennDiagram" # High-Resolution Venn and Euler Plots
+  )
 
 # Advanced Bioconductor Packages
 BP_level_2 <- c(
@@ -31,16 +34,19 @@ BP_level_2 <- c(
   "affycoretools",
   "clusterProfiler", # Package for ORA and GSEA in R
   "enrichplot", # Plot ORA and GSEA results
-  "pathview") # Download KEGG enrichment path locally
+  "pathview" # Download KEGG enrichment path locally
+  )
 
-# Specific-purpose Bioconductor Packages
+# Special-purpose Bioconductor Packages (annotation Data Bases)
 BP_level_3 <- c(
   "AnnotationDbi",
+  "org.Hs.eg.db",
   "hgu133a.db",
   "hgu133b.db",
   "hgu133plus2.db",
   "hugene10sttranscriptcluster.db",
-  "HsAgilentDesign026652.db")
+  "HsAgilentDesign026652.db"
+  )
 
 
 # ------ #
@@ -56,9 +62,12 @@ CP_level_1 <- c(
   "openxlsx", # Reading, Writing, and Editing of .xlsx (Excel) Files
   "gplots", # Heatmap with extensions - heatmap.2()
   "ggplot2",
-  "RColorBrewer")     # Color Palette for R
+  "RColorBrewer",     # Color Palette for R
+  "RSQLite" # SQLite interface for R
+  )
 
 CP_level_2 <- c(
+  "plyr", # Tools for Splitting, Applying and Combining Data
   "tidyverse",
   "rgl", # to build 3D charts - plot3d(), play3d()
   "magick", # for advanced image processing - scatter3d()
@@ -67,7 +76,8 @@ CP_level_2 <- c(
   "testthat",
   "knitr",
   "svDialogs", # Dialog Boxes for Windows and Linuxes
-  "ggnewscale")
+  "ggnewscale"
+  )
 
 
 CP_level_3 <- c()
